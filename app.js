5baa083cee163c9707b56dd7f80a17309b1213e2
Overall, Matthew <overallm@medamerica.com>
2016-04-09 21:00:53 -0600

    lights on-off almost

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready( function() {
     //This code will run after your page loads
-    $(".lights").mousedown(function() {
-        alert("Oh no!");
+    $(".light-left").hover(function() {
+      $('.lights-wrapper').addClass('light-left-on')
+    },function(){
+      $('.lights-wrapper').removeClass('light-left-on')
     });
 });
